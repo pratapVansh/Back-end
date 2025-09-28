cc4659d185d9857fb626a1160e4cbdbd25a9e184
pratapVansh <vanshprataps2004@gmail.com>
2025-09-28 19:53:52 +0530

    file upload cloudinary and all setup done

---
+++ middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp"); // folder where files will be saved
+  },
+  filename: function (req, file, cb) {
+    //const uniqueSuffix = Date.now() + '-' + Math.round(Math.random()*1E9)
+    cb(null, file.originalname);
+  }
+})
+
+export const upload = multer({
+    storage,
+})
