70dea3fa22b7074958c6215bed4e3105edc20603
pratapVansh <vanshprataps2004@gmail.com>
2025-10-02 14:32:26 +0530

    refactor: update getCurrentUser response format and ensure await in updateAccountDetails error correction

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -255,7 +255,7 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 
 const getCurrentUser = asyncHandler(async (req,res)=>{
     return res.status(200)
-    .json(200,req.user,"current user fetched Successfully")
+    .json(new ApiResponse(200,req.user,"current user fetched Successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async (req,res)=>{
@@ -265,7 +265,7 @@ const updateAccountDetails = asyncHandler(async (req,res)=>{
         throw new ApiError(400,"All fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set:{
@@ -310,7 +310,7 @@ const updateUserAvatar = asyncHandler(async(req,res)=>{
     )
 })
 
-const updateUserCoverImage = asyncHandler(async(req,res)=>{
+const updateUserCoverImage = asyncHandler(async(req,res)=>{ 
     const coverImageLocalPath = req.file?.path
 
     if(!coverImageLocalPath){
